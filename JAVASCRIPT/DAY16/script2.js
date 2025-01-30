81e762440f22fbac355fb31d3815da682542183f
srilekha1402 <2211cs010668@mallareddyuniversity.ac.in>
2025-01-30 21:17:36 +0530

    DAY16

---
+++ JAVASCRIPT/DAY16/script2.js
@@ -0,0 +1,16 @@
+ //import  {collegeName}  from "./script1.js";
+ import  {collegeName, Tasking as tasking}  from "./script1.js";
+ import total from "./script1.js"
+ 
+ tasking();
+ console.log(total);
+ 
+//  Form validations: Validations are like checkers for values that user enters in each field in your form.
+ 
+//    1. Create a signup form that  contains
+//   username(!empty),
+//   email(@, !empty), 
+//  password(should be 6) and 
+//  confirm password(same as password value, !empty), 
+//  once you click on submit button, a alert should be popped up on you screen "form is submitted successfully" if and only if all the validations are correct.
+ 
