c251cdcfcce027a1c2338a71474f7571ab9c7cef
srilekha1402 <2211cs010668@mallareddyuniversity.ac.in>
2025-01-31 20:52:41 +0530

    day2

---
+++ React/DAY1/app/src/component/Cbc.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+class Cbc extends React.Component{
+    render(){
+        return <h1>I am class based component</h1>
+    }
+}
+export default Cbc
---
+++ React/DAY1/app/src/component/Fbc.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+class Fbc extends React.Component{
+    render(){
+        return <h1>I am function based component</h1>
+    }
+}
+export default Fbc
