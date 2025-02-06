1a82864d21034f0c83ea2ca9410dcf142b5748b5
srilekha1402 <2211cs010668@mallareddyuniversity.ac.in>
2025-02-06 13:10:09 +0530

    react

---
+++ React/app/src/style/Comp2.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Comp2 = () => {
+  return (
+    <>
+      <section id='main'>
+        <h1>₹59.00 for 2 months of Premium Student</h1>
+        <h2>Only ₹59.00/month after. Cancel anytime.</h2>
+        <button>Get Premium</button>
+        <p>Offer available only to students at an accredited higher education institution. Offer not available to users who already tried<br/> Premium. Spotify Student Discount Offer <u>Terms and conditions</u> apply.</p>
+      </section>
+    </>
+  )
+}
+
+export default Comp2
---
+++ React/app/src/style/Inline.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Inline = () => {
+  return (
+    <>
+    <h1 style={{backgroundColor:"red",textAlign:"center",textDecoration:"underline"}}>Hello World</h1>
+   
+    </>
+  )
+}
+
+export default Inline
