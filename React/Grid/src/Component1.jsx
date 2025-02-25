0b802977a53b11aff35a3a91381f1f1c75dc02c3
srilekha1402 <2211cs010668@mallareddyuniversity.ac.in>
2025-02-25 12:49:27 +0530

    Grid

---
+++ React/Grid/src/Component1.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './component1.css'
+
+const Component1 = () => {
+  return (
+    <>
+      <div className='parent'>
+        <div className='header'>Header</div>
+        <div className='leftmenu'>Left Menu</div>
+        <div className='main'>Main</div>
+        <div className='rightmenu'>Right Menu</div>
+        <div className='footer'>Footer</div>
+      </div>
+    </>
+  )
+}
+
+export default Component1
